f7d368a390ab613c7c8044c1dfab2fa1cd0790e7
bomasen <bomasen93@gmail.com>
2023-12-24 13:01:44 +0100

    second version

--- src/main.ts
+++ src/main.ts
@@ -1,108 +1,78 @@
-//funciones generales
 let puntuacion: number = 0;
 
-const numeroAleatorio = (): number => Math.floor(Math.random() * 12 + 1);
+const numeroAleatorio = (): number => Math.floor(Math.random() * 10 + 1);
 
-//mostrar puntuación
 const mostrarPuntos = () => {
   const puntos = document.getElementById("puntos");
-  Boolean(puntos) && puntos instanceof HTMLSpanElement
+  puntos && puntos instanceof HTMLSpanElement
     ? (puntos.textContent = puntuacion.toString())
     : console.error("No se puede mostrar puntuación");
 };
 
-//generar numero de carta
-const numeroCarta = (numeroAleatorio: number) => {
-  if (numeroAleatorio === 8 || numeroAleatorio === 9) {
-    return numeroAleatorio + 2;
-  } else if (numeroAleatorio >= 1 && numeroAleatorio <= 12) {
-    return numeroAleatorio;
-  } else {
-    console.error("Error inesperado al generar número de carta");
-    return -1;
-  }
-};
+const numeroCarta = (numeroAleatorio: number): number =>
+  numeroAleatorio > 7 ? numeroAleatorio + 2 : numeroAleatorio;
 
-//sumar puntuación
-const sumarPuntos = (numeroCarta: number): number =>
-  numeroCarta <= 7 ? (puntuacion += numeroCarta) : (puntuacion += 0.5);
+const obtenerPuntos = (numeroCarta: number) =>
+  numeroCarta <= 7 ? numeroCarta : 0.5;
 
-//mostrar carta
-const mostrarCarta = (numeroCarta: number) => {
+const sumarPuntos = (puntos: number): number => puntuacion + puntos;
+
+const setPuntuacion = (nuevosPuntos: number) => (puntuacion = nuevosPuntos);
+
+const cambiarUrlCarta = (urlCarta: string) => {
   const carta = document.getElementById("carta");
-  if (Boolean(carta) && carta instanceof HTMLImageElement) {
-    switch (numeroCarta) {
-      case 0.01:
-        carta.src = "src/img/0_gameOver.jpg";
-        mostrarMensaje("💀!!GAME OVER!!💀");
-        break;
-      case 0.02:
-        carta.src = "src/img/win.png";
-        break;
-      case 1:
-        carta.src = "src/img/1_as-copas.jpg";
-        mostrarMensaje("+1 punto!!");
-        break;
-      case 2:
-        carta.src = "src/img/2_dos-copas.jpg";
-        mostrarMensaje("+2 puntos!!");
-        break;
-      case 3:
-        carta.src = "src/img/3_tres-copas.jpg";
-        mostrarMensaje("+3 puntos!!");
-        break;
-      case 4:
-        carta.src = "src/img/4_cuatro-copas.jpg";
-        mostrarMensaje("+4 puntos!!");
-        break;
-      case 5:
-        carta.src = "src/img/5_cinco-copas.jpg";
-        mostrarMensaje("+5 puntos!!");
-        break;
-      case 6:
-        carta.src = "src/img/6_seis-copas.jpg";
-        mostrarMensaje("+6 puntos!!");
-        break;
-      case 7:
-        carta.src = "src/img/7_siete-copas.jpg";
-        mostrarMensaje("+7 puntos!!");
-        break;
-      case 10:
-        carta.src = "src/img/10_sota-copas.jpg";
-        mostrarMensaje("+0,5 puntos!!");
-        break;
-      case 11:
-        carta.src = "src/img/11_caballo-copas.jpg";
-        mostrarMensaje("+0,5 puntos!!");
-        break;
-      case 12:
-        carta.src = "src/img/12_rey-copas.jpg";
-        mostrarMensaje("+0,5 puntos!!");
-        break;
-      default:
-        console.error("Número de carta inesperado");
-        break;
-    }
-  } else {
-    console.error("No se puede mostrar carta");
+  if (carta && carta instanceof HTMLImageElement) carta.src = urlCarta;
+};
+
+const obtenerUrlCarta = (numeroCarta: number): string => {
+  switch (numeroCarta) {
+    case 0.1:
+      return "src/img/0.1_gameOver.jpg";
+    case 1:
+      return "src/img/1_as-copas.jpg";
+    case 2:
+      return "src/img/2_dos-copas.jpg";
+    case 3:
+      return "src/img/3_tres-copas.jpg";
+    case 4:
+      return "src/img/4_cuatro-copas.jpg";
+    case 5:
+      return "src/img/5_cinco-copas.jpg";
+    case 6:
+      return "src/img/6_seis-copas.jpg";
+    case 7:
+      return "src/img/7_siete-copas.jpg";
+    case 10:
+      return "src/img/10_sota-copas.jpg";
+    case 11:
+      return "src/img/11_caballo-copas.jpg";
+    case 12:
+      return "src/img/12_rey-copas.jpg";
+    default:
+      return "Número de carta inesperado";
   }
 };
 
-//mostrar mensajes
 const mostrarMensaje = (texto: string) => {
   const mensaje = document.getElementById("mensaje");
-  Boolean(mensaje) && mensaje instanceof HTMLDivElement
+  mensaje && mensaje instanceof HTMLDivElement
     ? (mensaje.textContent = texto)
     : console.error("No se puede mostrar mensaje");
 };
 
-//ejecuciones handle de los eventos de los botones
+const mensajePuntos = (puntos: number) =>
+  mostrarMensaje(`Has sumado ${puntos} puntos!!`);
+
 const handlePedir = () => {
   const generarCarta: number = numeroCarta(numeroAleatorio());
-  mostrarCarta(generarCarta);
-  sumarPuntos(generarCarta);
+  const urlCarta: string = obtenerUrlCarta(generarCarta);
+  cambiarUrlCarta(urlCarta);
+  const puntos: number = obtenerPuntos(generarCarta);
+  const nuevoPuntos: number = sumarPuntos(puntos);
+  setPuntuacion(nuevoPuntos);
   mostrarPuntos();
-  gameOver();
+  mensajePuntos(puntos);
+  gestionPartida();
 };
 
 const handleSalir = () => mePlanto(puntuacion);
@@ -111,71 +81,64 @@ const handleNuevo = () => empezarDeNuevo();
 
 const handleVer = () => verResultadoSiguiente();
 
-//eventos
 const eventos = () => {
   const pedir = document.getElementById("pedir");
-  Boolean(pedir) && pedir instanceof HTMLButtonElement
+  pedir && pedir instanceof HTMLButtonElement
     ? pedir.addEventListener("click", handlePedir)
-    : console.error("Error en el botón pedir");
+    : console.error("Error en el botón Pedir");
 
   const salir = document.getElementById("salir");
-  Boolean(salir) && salir instanceof HTMLButtonElement
+  salir && salir instanceof HTMLButtonElement
     ? salir.addEventListener("click", handleSalir)
-    : console.error("Error en el botón salir");
+    : console.error("Error en el botón Salir");
 
   const nuevo = document.getElementById("nuevo");
-  Boolean(nuevo) && nuevo instanceof HTMLButtonElement
+  nuevo && nuevo instanceof HTMLButtonElement
     ? nuevo.addEventListener("click", handleNuevo)
-    : console.error("Error en el boton nuevo");
+    : console.error("Error en el boton Nuevo");
 
   const ver = document.getElementById("ver");
-  Boolean(ver) && ver instanceof HTMLButtonElement
+  ver && ver instanceof HTMLButtonElement
     ? ver.addEventListener("click", handleVer)
-    : console.error("Error en el boton ver");
+    : console.error("Error en el boton Ver");
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  mostrarPuntos();
-  eventos();
-  mostrarMensaje("!!Empieza el juego!!");
-});
-
-//Ocultar y mostrar botones
-const cambiarVistaBotones = () => {
+const visibilidadBotones = () => {
   const pedir = document.getElementById("pedir");
+  pedir && pedir instanceof HTMLButtonElement
+    ? pedir.remove()
+    : console.error("Error al cambiar visualización botón Pedir");
+
   const salir = document.getElementById("salir");
+  salir && salir instanceof HTMLButtonElement
+    ? salir.remove()
+    : console.error("Error al cambiar visualización botón Salir");
+
   const nuevo = document.getElementById("nuevo");
+  nuevo && nuevo instanceof HTMLButtonElement
+    ? (nuevo.style.display = "inline-block")
+    : console.error("Error al cambiar visualización botón Nuevo");
+
   const ver = document.getElementById("ver");
-  if (
-    Boolean(pedir) &&
-    pedir instanceof HTMLButtonElement &&
-    Boolean(salir) &&
-    salir instanceof HTMLButtonElement &&
-    Boolean(nuevo) &&
-    nuevo instanceof HTMLButtonElement &&
-    Boolean(ver) &&
-    ver instanceof HTMLButtonElement
-  ) {
-    pedir.remove();
-    salir.remove();
-    nuevo.style.display = "inline-block";
-    if (puntuacion < 7.5) ver.style.display = "inline-block";
-  } else {
-    console.error("Error al cambiar visualización de los botones");
-  }
+  ver && ver instanceof HTMLButtonElement && puntuacion < 7.5
+    ? (ver.style.display = "inline-block")
+    : console.error("Error al cambiar visualización botón Ver");
 };
 
-//fin de la partida
-const gameOver = () => {
+const gestionPartida = () => {
+  if (puntuacion === 7.5) {
+    mostrarMensaje("🏆¡Lo has clavado! ¡Enhorabuena!🏆");
+    visibilidadBotones();
+  }
   if (puntuacion > 7.5) {
-    cambiarVistaBotones();
-    mostrarCarta(0.01);
+    mostrarMensaje("💀!!GAME OVER!!💀");
+    visibilidadBotones();
+    const urlCarta: string = obtenerUrlCarta(0.1);
+    cambiarUrlCarta(urlCarta);
   }
 };
 
-//funcion plantarse
 const mePlanto = (puntuacion: number) => {
-  cambiarVistaBotones();
   switch (true) {
     case puntuacion <= 4:
       mostrarMensaje("Has sido muy conservador 🧐");
@@ -186,22 +149,31 @@ const mePlanto = (puntuacion: number) => {
     case puntuacion >= 6 && puntuacion <= 7:
       mostrarMensaje("Casi casi....🥈");
       break;
-    case puntuacion === 7.5:
-      mostrarMensaje("🏆¡Lo has clavado! ¡Enhorabuena!🏆");
-      mostrarCarta(0.02);
-      break;
     default:
       console.error("Número de puntuación inesperado");
-      break;
   }
+  visibilidadBotones();
 };
-//empezar otra partida
+
 const empezarDeNuevo = () => location.reload();
 
-//ver el resultado que tocaba despues
+let resetVerResultadosSiguiente = false;
 const verResultadoSiguiente = () => {
-  const generarCarta: number = numeroCarta(numeroAleatorio());
-  mostrarCarta(generarCarta);
-  sumarPuntos(generarCarta);
-  mostrarPuntos();
+  if (!resetVerResultadosSiguiente) {
+    const generarCarta: number = numeroCarta(numeroAleatorio());
+    const urlCarta: string = obtenerUrlCarta(generarCarta);
+    cambiarUrlCarta(urlCarta);
+    const puntos: number = obtenerPuntos(generarCarta);
+    const nuevoPuntos: number = sumarPuntos(puntos);
+    setPuntuacion(nuevoPuntos);
+    mostrarPuntos();
+    mensajePuntos(puntos);
+    resetVerResultadosSiguiente = true;
+  }
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  mostrarPuntos();
+  eventos();
+  mostrarMensaje("!!Empieza el juego!!");
+});
